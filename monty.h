752f8f39701f30be0a21e74b4015510b705d084e
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-21 16:13:07 -0700

    monty

--- monty.h
+++ monty.h
@@ -74,4 +74,5 @@ void f_div(stack_t **head, unsigned int count);
 void mul(stack_t **head, unsigned int count);
 void mod(stack_t **head, unsigned int count);
 void pchar(stack_t **head, unsigned int count);
+void pstr(stack_t **head, unsigned int count);
 #endif
