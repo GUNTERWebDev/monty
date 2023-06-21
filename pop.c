be9a506632f0e4cc989da298b443202c74d40679
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-21 10:58:42 -0700

    monty

--- pop.c
+++ pop.c
@@ -3,7 +3,7 @@ void pop(stack_t **head, unsigned int count)
 {
 	stack_t *next;
 
-	if (head == 0)
+	if (*head == 0)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", count);
 		exit(EXIT_FAILURE);
