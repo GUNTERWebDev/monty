ae915e5cb1f0d28b1a6a29abc09d7d907ff966d8
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-21 12:18:54 -0700

    monty

--- op.c
+++ op.c
@@ -3,22 +3,21 @@
 void sub(stack_t **head, unsigned int count)
 {
 	stack_t *h;
-	int len = 0;
+	int len = 0, sum;
 
 	h = *head;
 
-	while (h)
-	{
+	for (len = 0; h != NULL; len++)
 		h = h->next;
-		len++;
-	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", count);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = (*head)->next;
-	h->n -= (*head)->n;
-	pop(head, count);
+	h = *head;
+	sum = h->next->n - h->n;
+	h->next->n = sum;
+	*head = h->next;
+	free(h);
 }
