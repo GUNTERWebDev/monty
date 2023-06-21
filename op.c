ecb313a3a3f4aa9721615576b3dce88084d470d8
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-21 12:54:22 -0700

    monty

--- op.c
+++ op.c
@@ -11,10 +11,7 @@ void sub(stack_t **head, unsigned int count)
 		h = h->next;
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", count);
-		fclose(diff.fd);
-		free(diff.cmd);
-		free_stack(*head);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", count);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
@@ -23,3 +20,28 @@ void sub(stack_t **head, unsigned int count)
 	*head = h->next;
 	free(h);
 }
+/*
+void div(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	int len = 0, div;
+
+        h = *head;
+
+        for (len = 0; h != NULL; len++)
+                h = h->next;
+        if (len < 2)
+        {
+                fprintf(stderr, "L%d: can't add, stack too short\n", count);
+                exit(EXIT_FAILURE);
+        }
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L<line_number>: division by zero\n", count);
+		exit(EXIT_FAILURE);
+	}
+	div = h->prev->n / h->n;
+        h->next->n = div;
+        *head = h->next;
+        free(h);
+}*/
